363a1e1ae84c4c00f065de9954073a5071df095e
Ikorrin123 <61454547+Ikorrin123@users.noreply.github.com>
2020-03-09 19:00:28 +0100

    Add files via upload
    zapis

---
+++ zaczynamy/zaczynamy/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace zaczynamy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OtworzButton = new System.Windows.Forms.Button();
+            this.zapiszButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // OtworzButton
+            // 
+            this.OtworzButton.Location = new System.Drawing.Point(12, 22);
+            this.OtworzButton.Name = "OtworzButton";
+            this.OtworzButton.Size = new System.Drawing.Size(75, 23);
+            this.OtworzButton.TabIndex = 0;
+            this.OtworzButton.Text = "otworz";
+            this.OtworzButton.UseVisualStyleBackColor = true;
+            this.OtworzButton.Click += new System.EventHandler(this.OtworzButton_Click);
+            // 
+            // zapiszButton
+            // 
+            this.zapiszButton.Location = new System.Drawing.Point(115, 22);
+            this.zapiszButton.Name = "zapiszButton";
+            this.zapiszButton.Size = new System.Drawing.Size(75, 23);
+            this.zapiszButton.TabIndex = 1;
+            this.zapiszButton.Text = "zapisz";
+            this.zapiszButton.UseVisualStyleBackColor = true;
+            this.zapiszButton.Click += new System.EventHandler(this.zapiszButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 72);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(609, 259);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "JPG|.jpg|PNG|.png|BMP|*.bmp";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.zapiszButton);
+            this.Controls.Add(this.OtworzButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "obrazek";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OtworzButton;
+        private System.Windows.Forms.Button zapiszButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ zaczynamy/zaczynamy/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zaczynamy
+{
+    public partial class Form1 : Form
+    {
+        Image obrazek;//globalna zmienna do przechywcenia obrazku z dysku
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OtworzButton_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = openFileDialog1.ShowDialog();//otwarcie okna dialogowoego i wybranie
+            if(dr == DialogResult.OK)//sprawdzanie czy wybrano sprawny plik
+            {
+                obrazek = Image.FromFile(openFileDialog1.FileName);//przypisanie wybranego pliku do zmiennej
+                pictureBox1.Image = obrazek;//wstawienie obrazka do picture boxa
+            }
+        }
+
+        private void zapiszButton_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = saveFileDialog1.ShowDialog();
+            if (dr == DialogResult.OK)//sprawdzanie czy wybrano sprawny plik
+            {
+                if (obrazek != null)
+                {
+                    if (saveFileDialog1.FileName.Substring(saveFileDialog1.FileName.Length - 3) == "jpg")
+                    {
+                        obrazek.Save(saveFileDialog1.FileName, ImageFormat.Jpeg);
+
+                    }
+                    else if (saveFileDialog1.FileName.Substring(saveFileDialog1.FileName.Length - 3) == "png")
+                    {
+                        obrazek.Save(saveFileDialog1.FileName, ImageFormat.Png);
+                    }
+                    else (saveFileDialog1.FileName.Substring(saveFileDialog1.FileName.Length - 3) == "bmp")
+                    {
+                        obrazek.Save(saveFileDialog1.FileName, ImageFormat.Bmp);
+                    }
+                        
+                    
+                } 
+            }
+        }
+    }
+}
